531c089756441636330411519f5daf9e91b3e9a8
Brandon Perfetti <brandon@brandonperfetti.com>
2024-07-30 13:41:40 -0700

    Start work on The Question Editor

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Editor } from "@tinymce/tinymce-react";
+import { useRef } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -18,11 +20,14 @@ import { Input } from "@/components/ui/input";
 import { QuestionsSchema } from "@/lib/validations";
 
 const Question = () => {
+  const editorRef = useRef(null);
+
+  // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
     defaultValues: {
       title: "",
-      explination: "",
+      explanation: "",
       tags: [],
     },
   });
@@ -63,20 +68,49 @@ const Question = () => {
         />
         <FormField
           control={form.control}
-          name="explination"
+          name="explanation"
           render={({ field }) => (
             <FormItem className="flex w-full flex-col gap-3">
               <FormLabel className="paragraph-semibold text-dark400_light800">
-                Detailed explination of your problem
+                Detailed explanation of your problem
                 <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl className="mt-3.5">
-                {/* TODO: Add a rich text editor here */}
-                {/* <Input
-                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                  placeholder="shadcn"
-                  {...field}
-                /> */}
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  onInit={(_evt, editor) => {
+                    // @ts-ignore
+                    // Required to access the editor instance
+                    editorRef.current = editor;
+                  }}
+                  initialValue=""
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      "advlist",
+                      "anchor",
+                      "autolink",
+                      "charmap",
+                      "codesample",
+                      "fullscreen",
+                      "image",
+                      "insertdatetime",
+                      "link",
+                      "lists",
+                      "media",
+                      "preview",
+                      "searchreplace",
+                      "table",
+                      "visualblocks",
+                    ],
+                    toolbar:
+                      "undo redo | blocks | " +
+                      "codesample | bold italic forecolor | alignleft aligncenter " +
+                      "alignright alignjustify | bullist numlist",
+                    content_style: "body { font-family:Inter; font-size:16px }",
+                  }}
+                />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Introduce the problem and expand on what you put in the title.
