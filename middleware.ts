183d57c83a02e6e6eda153c53c115c5ac1d58754
Brandon Perfetti <brandon@brandonperfetti.com>
2024-07-31 14:26:23 -0700

    Update Public Routes and fix WEBHOOK_SECRET

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,19 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/", "/api/webhook"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/api/webhook",
+  "/question/:id",
+  "/tags",
+  "/tags/:id",
+  "/profile/:id",
+  "/community",
+  "/jobs",
+  "/api/chatgpt",
+  "/ask-question(.*)",
+]);
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
