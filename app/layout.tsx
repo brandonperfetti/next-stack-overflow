0652bf259e0a814aee0f3fb3391fe7d90be3b0fe
Brandon Perfetti <brandon@brandonperfetti.com>
2024-07-31 13:47:34 -0700

    Fis ESLint errors.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+/* eslint-disable camelcase */
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
-// eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 
 import type { Metadata } from "next";
