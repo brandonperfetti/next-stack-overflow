c1ee3bd560b60e465c2528a9ba4006670ae0eaf9
Brandon Perfetti <brandon@brandonperfetti.com>
2024-08-01 12:07:23 -0700

    Implement Create Answer action

---
+++ lib/actions/answer.action.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import Answer from "@/database/answer.model";
+import Question from "@/database/question.model";
+import { revalidatePath } from "next/cache";
+import { connectToDatabase } from "../mongoose";
+import { CreateAnswerParams } from "./shared.types";
+
+export async function createAnswer(params: CreateAnswerParams) {
+  connectToDatabase();
+
+  try {
+    const { content, author, question, path } = params;
+
+    const newAnswer = new Answer({
+      content,
+      author,
+      question,
+    });
+    // Add the answer to the questions's answer array
+    await Question.findByIdAndUpdate(question, {
+      $push: { answers: newAnswer._id },
+    });
+    // TODO: Add interaction...
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
