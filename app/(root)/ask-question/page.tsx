a31b97fa729721b0dc740f40996a3003b8dc4b05
Brandon Perfetti <brandon@brandonperfetti.com>
2024-08-10 15:36:45 -0700

    Convert Question tag input into searchable combobox. Enforce 3 tag limitation.

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,4 +1,5 @@
 import Question from "@/components/forms/Question";
+import { getAllTags } from "@/lib/actions/tag.action";
 import { getUserById } from "@/lib/actions/user.action";
 import { auth } from "@clerk/nextjs/server";
 import type { Metadata } from "next";
@@ -12,12 +13,18 @@ const AskQuestionPage = async () => {
   const { userId } = auth();
   if (!userId) redirect("/sign-in");
   const mongoUser = await getUserById({ userId });
-  // console.log(mongoUser);
+
+  const { tags } = await getAllTags({ filter: "name" });
+  const tagList = tags.map((tag: any) => tag.name);
+  
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
       <div className="mt-9">
-        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+        <Question
+          mongoUserId={JSON.stringify(mongoUser._id)}
+          allTags={Array.isArray(tagList) ? tagList : []}
+        />
       </div>
     </div>
   );
--- app/(root)/question/[id]/edit/page.tsx
+++ app/(root)/question/[id]/edit/page.tsx
@@ -1,5 +1,6 @@
 import Question from "@/components/forms/Question";
 import { getQuestionById } from "@/lib/actions/question.action";
+import { getAllTags } from "@/lib/actions/tag.action";
 import { getUserById } from "@/lib/actions/user.action";
 import { ParamsProps } from "@/types";
 import { auth } from "@clerk/nextjs/server";
@@ -13,6 +14,9 @@ const EditQuestionPage = async ({ params }: ParamsProps) => {
   const mongoUserId = mongoUser._id.toString();
   const result = await getQuestionById({ questionId: params.id });
 
+  const { tags } = await getAllTags({ filter: "name" });
+  const tagList = tags.map((tag: any) => tag.name);
+
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">Edit Question</h1>
@@ -22,6 +26,7 @@ const EditQuestionPage = async ({ params }: ParamsProps) => {
           type="Edit"
           mongoUserId={mongoUserId}
           questionDetails={JSON.stringify(result)}
+          allTags={Array.isArray(tagList) ? tagList : []}
         />
       </div>
     </>
