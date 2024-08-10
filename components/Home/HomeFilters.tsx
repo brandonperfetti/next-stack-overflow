75a7679daf7917a3593069b449dcae1f22182701
Brandon Perfetti <brandon@brandonperfetti.com>
2024-08-10 12:23:21 -0700

    Hide Recommended filter on HomePage if there is no user loged in.

--- components/Home/HomeFilters.tsx
+++ components/Home/HomeFilters.tsx
@@ -1,12 +1,16 @@
 "use client";
 
-import { HomePageFilters } from "@/constants/filters";
+import { AuthedHomePageFilters, HomePageFilters } from "@/constants/filters";
 import { formUrlQuery } from "@/lib/utils";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 import { Button } from "../ui/button";
 
-const HomeFilters = () => {
+interface HomeFiltersProps {
+  userId?: string | null;
+}
+
+const HomeFilters = ({ userId }: HomeFiltersProps) => {
   const searchParams = useSearchParams();
   const [active, setActive] = useState("");
   const router = useRouter();
@@ -37,19 +41,39 @@ const HomeFilters = () => {
 
   return (
     <div className="mt-10 hidden flex-wrap gap-3 md:flex">
-      {HomePageFilters.map((item) => (
-        <Button
-          key={item.value}
-          onClick={() => handleTypeClick(item.value)}
-          className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
-            active === item.value
-              ? "bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500 dark:hover:bg-dark-400"
-              : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"
-          }`}
-        >
-          {item.name}
-        </Button>
-      ))}
+      {!userId ? (
+        <>
+          {HomePageFilters.map((item) => (
+            <Button
+              key={item.value}
+              onClick={() => handleTypeClick(item.value)}
+              className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
+                active === item.value
+                  ? "bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500 dark:hover:bg-dark-400"
+                  : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"
+              }`}
+            >
+              {item.name}
+            </Button>
+          ))}
+        </>
+      ) : (
+        <>
+          {AuthedHomePageFilters.map((item) => (
+            <Button
+              key={item.value}
+              onClick={() => handleTypeClick(item.value)}
+              className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
+                active === item.value
+                  ? "bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500 dark:hover:bg-dark-400"
+                  : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"
+              }`}
+            >
+              {item.name}
+            </Button>
+          ))}
+        </>
+      )}
     </div>
   );
 };
